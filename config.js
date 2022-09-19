ee5ae7cc70d402b680ad5d93642d81f9c7d28b10
‘rin’ <785879986@qq.com>
2022-09-19 16:54:07 +0800

    已经可以下载模板了

--- config.js
+++ config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  framwork: ['express', 'koa', 'egg']
+  // 可选择的框架
+  framwork: ['express', 'koa', 'egg'],
+  // 框架对应的下载地址
+  framworkUrl: {
+    express: 'git@github.com:Rinsann/express-template.git',
+    koa: 'git@github.com:Rinsann/koa-template.git',
+    egg: 'git@github.com:Rinsann/egg-template.git'
+  }
 }
